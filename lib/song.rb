70ec9dbe90f914a15f650847519573fe93c1b497
Salem Asrat <salemasrat@gmail.com>
2019-11-14 03:45:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,17 @@ class Song
     
   def self.all 
     @@all 
-  end   
+  end  
+  
+  
+  def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+end
     
-end   
+  
