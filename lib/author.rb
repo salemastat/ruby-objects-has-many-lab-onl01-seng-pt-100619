9a024a7e527369759acd9e196c0e4a527dec0615
Salem Asrat <salemasrat@gmail.com>
2019-11-14 02:56:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,32 @@
-class Author 
-  attr_accessor :name, :posts 
-  
-  def initialize(name)
-    @name = name 
-    @posts = []
-  end   
-  
-  def posts 
-   Post.all.select do |Post|
-     post.author == self 
-   end 
- end 
- 
- def add_post(post)
-   post.author = self 
- end  
- 
- def add_post_by_name(post_name)
-   title = Post.new(post_name)
-   add_post(title)
- end    
 
-  def self.post_count 
-    Post.all.count
+class Author 
+  attr_accessor :name
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
   end
 
 end
