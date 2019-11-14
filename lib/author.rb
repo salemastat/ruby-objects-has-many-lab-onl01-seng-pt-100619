737f43f16a03e60ced4a0a8481637a98b5ddb524
Salem Asrat <salemasrat@gmail.com>
2019-11-14 03:22:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,9 +19,7 @@ class Author
 
   def add_post_by_title(post_title)
     post = Post.new(post_title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+    
   end
 
   def self.post_count
