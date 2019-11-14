3c86443e3f69b63fb0332789a8c85515cc60d172
Salem Asrat <salemasrat@gmail.com>
2019-11-14 03:46:29 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+
+class Author 
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+   Post.all.select do |post|
+     post.author == self 
+   end 
+ end 
+ 
+  def add_post(post)
+   post.author = self 
+ end  
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+     add_post(post)
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
